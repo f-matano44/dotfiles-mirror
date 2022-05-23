4cc02952702f6953461fcad12857b73bd43995bd
Fumiyoshi MATANO <f.matano44@e.email>
2022-05-23 13:28:12 +0900

    add git setting

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ ln -s $CURRENT/vim ~/.vim
 ln -s $CURRENT/vimrc ~/.vimrc
 ln -s $CURRENT/zshrc ~/.zshrc
 ln -s $CURRENT/zprofile ~/.zprofile
+ln -s $CURRENT/gitconfig ~/.gitconfig
+ln -s $CURRENT/gitignore_global ~/.gitignore_global
