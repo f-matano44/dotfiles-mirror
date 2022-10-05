3528daa8a2f3b3e97eab7005fb4a9894c85d331a
Fumiyoshi MATANO <f_matano44@yahoo.co.jp>
2022-10-05 15:03:16 +0900

    add asdf

--- fish/config.fish
+++ fish/config.fish
@@ -28,3 +28,5 @@ alias javacu='javac -J-Dfile.encoding=UTF-8'
 alias javau='java -Dfile.encoding=UTF-8'
 alias targz='tar -zcvf' # targz filename.tar.gz dirname
 alias untargz='tar -zxvf' # untargz filename.tar.gz
+
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
