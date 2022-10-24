f0028f86b2f05a3841c0fa71279ab5d75ccc28fc
Fumiyoshi MATANO <f_matano44@yahoo.co.jp>
2022-10-24 15:08:54 +0900

    保存し忘れ

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,7 @@
+# Commands to run in interactive sessions can go here
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    set fish_greeting # remove intro message
 end
-
-# remove intro message
-set fish_greeting
 
 # homebrew
 # ref: https://blog.mksc.jp/contents/apple-silicon/
@@ -13,19 +11,22 @@ alias 86brew='arch -x86_64 /usr/local/bin/brew'
 ## AppleSilicon
 eval (/opt/homebrew/bin/brew shellenv)
 
-# PATH
+# Python
+alias python='python3'
+alias pip='pip3'
 set PATH ~/Library/Python/3.8/bin $PATH # pipenv
 
-# alias
+# Java
+alias javacu='javac -J-Dfile.encoding=UTF-8'
+alias javau='java -Dfile.encoding=UTF-8'
+
+# asdf
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# other alias
 alias la='ls -a'
 alias halt='shutdown -h now'
 alias reboot='shutdown -r now'
 alias えぃｔ='exit'
-alias python='python3'
-alias pip='pip3'
-alias javacu='javac -J-Dfile.encoding=UTF-8'
-alias javau='java -Dfile.encoding=UTF-8'
 alias targz='tar -zcvf' # targz filename.tar.gz dirname
 alias untargz='tar -zxvf' # untargz filename.tar.gz
-
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
