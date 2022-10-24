e94d2c88ac72a1ac465c63d669f17c9283c90f85
Fumiyoshi MATANO <f_matano44@yahoo.co.jp>
2022-10-24 15:36:43 +0900

    alias -> abbr

--- fish/config.fish
+++ fish/config.fish
@@ -7,26 +7,26 @@ end
 # ref: https://blog.mksc.jp/contents/apple-silicon/
 ## Intel
 eval (/usr/local/bin/brew shellenv)
-alias 86brew='arch -x86_64 /usr/local/bin/brew'
+alias 86brew 'arch -x86_64 /usr/local/bin/brew'
 ## AppleSilicon
 eval (/opt/homebrew/bin/brew shellenv)
 
 # Python
-alias python='python3'
-alias pip='pip3'
+alias python 'python3'
+alias pip 'pip3'
 set PATH ~/Library/Python/3.8/bin $PATH # pipenv
 
 # Java
-alias javacu='javac -J-Dfile.encoding=UTF-8'
-alias javau='java -Dfile.encoding=UTF-8'
+alias javacu 'javac -J-Dfile.encoding=UTF-8'
+alias javau 'java -Dfile.encoding=UTF-8'
 
 # asdf
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 # other alias
-alias la='ls -a'
-alias halt='shutdown -h now'
-alias reboot='shutdown -r now'
-alias えぃｔ='exit'
-alias targz='tar -zcvf' # targz filename.tar.gz dirname
-alias untargz='tar -zxvf' # untargz filename.tar.gz
+abbr la 'ls -a'
+alias halt 'shutdown -h now'
+alias reboot 'shutdown -r now'
+alias えぃｔ 'exit'
+abbr targz 'tar -zcvf' # targz filename.tar.gz dirname
+abbr untargz 'tar -zxvf' # untargz filename.tar.gz
