22f50ef7333a7dace696473f7b9d6f00270ac4e4
Fumiyoshi MATANO <f_matano44@yahoo.co.jp>
2023-01-14 22:03:46 +0900

    Python java docker

--- fish/config.fish
+++ fish/config.fish
@@ -16,12 +16,28 @@ alias python 'python3'
 alias pip 'pip3'
 set PATH ~/Library/Python/3.8/bin $PATH # pipenv
 
+# Python x86_64
+## Python has been installed as
+##   /usr/local/bin/python3.8
+##
+## Unversioned and major-versioned symlinks `python`, `python3`, `python-config`, `python3-config`, `pip`, `pip3`, etc. pointing to
+## `python3.8`, `python3.8-config`, `pip3.8`, etc., respectively, have been installed into
+##   /usr/local/opt/python@3.8/libexec/bin
+alias 86python 'arch -x86_64 /usr/local/bin/python3.8'
+alias 86pip 'arch -x86_64 /usr/local/bin/python3.8 -m pip'
+
 # Java
+fish_add_path /opt/homebrew/opt/openjdk@11/bin
+set -gx CPPFLAGS "-I/opt/homebrew/opt/openjdk@11/include"
 alias javacu 'javac -J-Dfile.encoding=UTF-8'
 alias javau 'java -Dfile.encoding=UTF-8'
 
 # asdf
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# Docker
+abbr d "docker"
+abbr dc "docker-compose"
 
 # other alias
 abbr la 'ls -a'
