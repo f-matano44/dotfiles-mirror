f5d879592d3bfed8159e3172b1804c900cc36f82
Fumiyoshi MATANO <f_matano44@yahoo.co.jp>
2023-06-04 15:23:00 +0900

    modify comment

--- fish/config.fish
+++ fish/config.fish
@@ -13,7 +13,6 @@ eval (/opt/homebrew/bin/brew shellenv)
 
 # Python (miniconda3)
 fish_add_path ~/miniconda3/bin
-#conda config --set auto_activate_base false
 
 # Java
 fish_add_path ~/Applications/apache-ant-1.10.13/bin
@@ -21,8 +20,6 @@ fish_add_path ~/Applications/apache-maven-3.9.2/bin
 fish_add_path /opt/homebrew/opt/openjdk@17/bin
 set JAVA_HOME /opt/homebrew/opt/openjdk@17
 set -gx CPPFLAGS "-I/opt/homebrew/opt/openjdk@17/include"
-# alias javacu 'javac -J-Dfile.encoding=UTF-8'
-# alias javau 'java -Dfile.encoding=UTF-8'
 
 # asdf
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
