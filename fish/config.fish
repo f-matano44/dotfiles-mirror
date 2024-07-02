b704e756a804f6e486995c9f80e31658b3ecf97b
Fumiyoshi MATANO <f_matano44@yahoo.co.jp>
2024-07-02 21:19:14 +0900

    miniconda -> miniforge

--- fish/config.fish
+++ fish/config.fish
@@ -23,22 +23,27 @@ abbr targz 'tar -zcvf' # targz filename.tar.gz dirname
 abbr untargz 'tar -zxvf' # untargz filename.tar.gz
 alias matlab-cli '/Applications/MATLAB_R2023b.app/bin/matlab -nodesktop'
 
+# atcoder
+alias atjava "mkdir a b c d && \
+    cp ~/dotfiles/Main.java ./a/Main.java && \
+    cp ~/dotfiles/Main.java ./b/Main.java && \
+    cp ~/dotfiles/Main.java ./c/Main.java && \
+    cp ~/dotfiles/Main.java ./d/Main.java &&"
+# abbr attest "oj t -c \"java Main.java\" -d ./test/"
+# abbr atsubm4 "acc submit Main.java -- -l 4005"
+# abbr atsubm5 "acc submit Main.java -- -l 5005"
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
-    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
 else
-    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    if test -f "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+        set -x PATH "/opt/homebrew/Caskroom/miniforge/base/bin" $PATH
     end
 end
 # <<< conda initialize <<<
 conda activate p310
 
-# atcoder
-alias atjava "cat ~/dotfiles/Main.java > ./Main.java"
-# abbr attest "oj t -c \"java Main.java\" -d ./test/"
-# abbr atsubm4 "acc submit Main.java -- -l 4005"
-# abbr atsubm5 "acc submit Main.java -- -l 5005"
