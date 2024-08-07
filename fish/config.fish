486a2ac07d44a9da4938bb9c1194304086a7aa9e
Fumiyoshi MATANO <cs232038@meiji.ac.jp>
2024-08-07 16:56:17 +0900

    全体的に整理

--- fish/config.fish
+++ fish/config.fish
@@ -6,14 +6,41 @@ if status is-interactive
     set fish_greeting # remove intro message
 end
 
-# homebrew
-# if use x64: https://blog.mksc.jp/contents/apple-silicon/
-eval (/opt/homebrew/bin/brew shellenv)
+switch (uname)
+    case "Darwin"
+        # homebrew
+        # if use x64: https://blog.mksc.jp/contents/apple-silicon/
+        eval (/opt/homebrew/bin/brew shellenv)
 
-# Java
-set -gx JAVA_HOME (/usr/libexec/java_home -v "17")
-fish_add_path $JAVA_HOME/bin
-set -gx CPPFLAGS "-I$JAVA_HOME/include"
+        # Java
+        set -gx JAVA_HOME (/usr/libexec/java_home -v "17")
+        fish_add_path $JAVA_HOME/bin
+        set -gx CPPFLAGS "-I$JAVA_HOME/include"
+
+        # MATLAB
+        alias matlab-cli '/Applications/MATLAB_R2023b.app/bin/matlab -nodesktop'
+
+        # LaTeX
+        fish_add_path /usr/local/texlive/2024basic/bin/universal-darwin
+
+        # >>> conda initialize >>>
+        # !! Contents within this block are managed by 'conda init' !!
+        if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
+            eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
+        else
+            if test -f "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+                . "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+            else
+                set -x PATH "/opt/homebrew/Caskroom/miniforge/base/bin" $PATH
+            end
+        end
+        # <<< conda initialize <<<
+        conda activate p310
+
+    case "Linux"
+        echo "This is Linux!"
+end
+
 
 # other alias
 alias halt 'shutdown -h now'
@@ -21,31 +48,13 @@ alias reboot 'shutdown -r now'
 alias えぃｔ 'exit'
 abbr targz 'tar -zcvf' # targz filename.tar.gz dirname
 abbr untargz 'tar -zxvf' # untargz filename.tar.gz
-alias matlab-cli '/Applications/MATLAB_R2023b.app/bin/matlab -nodesktop'
 
 # atcoder
 alias atjava "mkdir a b c d && \
-    cp ~/dotfiles/Main.java ./a/Main.java && \
-    cp ~/dotfiles/Main.java ./b/Main.java && \
-    cp ~/dotfiles/Main.java ./c/Main.java && \
-    cp ~/dotfiles/Main.java ./d/Main.java &&"
+    cp ~/dotfiles/atcoder/Main.java ./a/Main.java && \
+    cp ~/dotfiles/atcoder/Main.java ./b/Main.java && \
+    cp ~/dotfiles/atcoder/Main.java ./c/Main.java && \
+    cp ~/dotfiles/atcoder/Main.java ./d/Main.java &&"
 # abbr attest "oj t -c \"java Main.java\" -d ./test/"
 # abbr atsubm4 "acc submit Main.java -- -l 4005"
 # abbr atsubm5 "acc submit Main.java -- -l 5005"
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
-    eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
-        . "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/opt/homebrew/Caskroom/miniforge/base/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
-conda activate p310
-
-# LaTeX
-fish_add_path /usr/local/texlive/2024basic/bin/universal-darwin
--- setup.sh
+++ setup.sh
@@ -1,13 +1,46 @@
 #!/bin/sh
 
+set -e
+
 CURRENT=`pwd`
 
+# make trush
+date_str=$(date +%Y%m%d%H%M%S)
+random_str=$(LC_ALL=C tr -dc 'a-zA-Z0-9' < /dev/random | fold -w 8 | head -n 1)
+date_ramdom=old_${date_str}_${random_str}
+mkdir ${CURRENT}/${date_ramdom}
+
+# make dir
+if [ ! -d ${HOME}/.vim/colors ]; then
+    mkdir -p ${HOME}/.vim/colors
+fi
+if [ ! -d ${HOME}/.vim/colors ]; then
+    mkdir -p ${HOME}/.config/fish/
+fi
+
 # create symbolic link
-mkdir ~/.vim && mkdir ~/.vim/colors
-ln -s $CURRENT/vim/lucario.vim ~/.vim/colors/lucario.vim
-ln -s $CURRENT/vim/vimrc ~/.vimrc
-ln -s $CURRENT/zsh/zshrc ~/.zshrc
-ln -s $CURRENT/zsh/zprofile ~/.zprofile
-ln -s $CURRENT/git/gitconfig ~/.gitconfig
-ln -s $CURRENT/git/gitignore_global ~/.gitignore_global
-ln -s $CURRENT/fish/config.fish ~/.config/fish/config.fish
+## lucario
+if [ -f ${HOME}/.vim/colors/lucario.vim ]; then
+    mv ${HOME}/.vim/colors/lucario.vim ${date_ramdom}/lucario.vim
+fi
+ln -s ${CURRENT}/vim/lucario.vim ${HOME}/.vim/colors/lucario.vim
+## vimrc
+if [ -f "${HOME}/.vimrc" ]; then
+    mv ${HOME}/.vimrc ${date_ramdom}/vimrc
+fi
+ln -s ${CURRENT}/vim/vimrc ${HOME}/.vimrc
+## gitconfig
+if [ -f "${HOME}/.gitconfig" ]; then
+    mv ${HOME}/.gitconfig ${date_ramdom}/gitconfig
+fi
+ln -s ${CURRENT}/git/gitconfig ${HOME}/.gitconfig
+## gitignore_global
+if [ -f "${HOME}/.gitignore_global" ]; then
+    mv ${HOME}/.gitignore_global ${date_ramdom}/gitignore_global
+fi
+ln -s ${CURRENT}/git/gitignore_global ${HOME}/.gitignore_global
+## fish_config
+if [ -f "${HOME}/.config/fish/config.fish" ]; then
+    mv ${HOME}/.config/fish/config.fish ${date_ramdom}/config.fish
+fi
+ln -s ${CURRENT}/fish/config.fish ${HOME}/.config/fish/config.fish
