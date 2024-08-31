6c1deeacb21bda35809d419e6be6b0ae50070e62
Fumiyoshi MATANO <cs232038@meiji.ac.jp>
2024-08-31 13:04:16 +0900

    add: install code for codium extension

--- setup.sh
+++ setup.sh
@@ -4,11 +4,13 @@ set -e
 
 CURRENT=`pwd`
 
+
 # make trush
 date_str=$(date +%Y%m%d%H%M%S)
 random_str=$(LC_ALL=C tr -dc 'a-zA-Z0-9' < /dev/random | fold -w 8 | head -n 1)
 date_ramdom=old_${date_str}_${random_str}
 mkdir ${CURRENT}/${date_ramdom}
+
 
 # make dir
 if [ ! -d ${HOME}/.vim/colors ]; then
@@ -17,6 +19,7 @@ fi
 if [ ! -d ${HOME}/.vim/colors ]; then
     mkdir -p ${HOME}/.config/fish/
 fi
+
 
 # create symbolic link
 ## lucario
@@ -44,3 +47,44 @@ if [ -f "${HOME}/.config/fish/config.fish" ]; then
     mv ${HOME}/.config/fish/config.fish ${date_ramdom}/config.fish
 fi
 ln -s ${CURRENT}/fish/config.fish ${HOME}/.config/fish/config.fish
+
+
+# install codium extension
+extensionID=(
+    "bungcip.better-toml"
+    "ms-python.black-formatter"
+    "pixl-garden.BongoCat"
+    "shengchen.vscode-checkstyle"
+    "streetsidesoftware.code-spell-checker"
+    "janisdd.vscode-edit-csv"
+    "usernamehw.errorlens"
+    "bmalehorn.vscode-fish"
+    "ms-python.flake8"
+    "eamodio.gitlens"
+    "oderwat.indent-rainbow"
+    "ms-python.isort"
+    "MS-CEINTL.vscode-language-pack-ja"
+    "wmanth.jar-viewer"
+    "ms-toolsai.jupyter"
+    "ms-toolsai.vscode-jupyter-cell-tags"
+    "ms-toolsai.jupyter-keymap"
+    "ms-toolsai.jupyter-renderers"
+    "ms-toolsai.vscode-jupyter-slideshow"
+    "fwcd.kotlin"
+    "redhat.java"
+    "ritwickdey.LiveServer"
+    "ms-python.mypy-type-checker"
+    "richterger.perl"
+    "OrhunUlusahin.praatvscode"
+    "ms-python.python"
+    "ms-python.debugpy"
+    "REditorSupport.r"
+    "mechatroner.rainbow-csv"
+    "redhat.fabric8-analytics"
+    "jock.svg"
+    "vscode-icons-team.vscode-icons"
+    "tomoki1207.pdf"
+)
+for extID in "${extensionID[@]}"; do
+    codium --install-extension "${extID}"
+done
