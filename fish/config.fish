301b578ff03b6f17ad2eaade06d0a465689f5c0c
Fumiyoshi MATANO <cs232038@meiji.ac.jp>
2024-09-17 20:05:14 +0900

    mod

--- fish/config.fish
+++ fish/config.fish
@@ -23,18 +23,8 @@ switch (uname)
         # LaTeX
         # fish_add_path /usr/local/texlive/2024basic/bin/universal-darwin
 
-        # >>> conda initialize >>>
-        # !! Contents within this block are managed by 'conda init' !!
-        if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
-            eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
-        else
-            if test -f "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
-                . "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
-            else
-                set -x PATH "/opt/homebrew/Caskroom/miniforge/base/bin" $PATH
-            end
-        end
-        # <<< conda initialize <<<
+        # conda
+        eval "$(/Users/fumi/.miniforge3/bin/conda shell.fish hook)"
         conda activate p310
 
     case "Linux"  # Linux
