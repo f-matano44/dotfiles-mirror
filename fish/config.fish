de23994162645283b4fc1d17572d25d3e4c13640
Fumiyoshi MATANO <fumiyoshi.matano@gmail.com>
2025-03-25 16:49:46 +0900

    rm rocm

--- fish/config.fish
+++ fish/config.fish
@@ -43,8 +43,8 @@ switch (uname)
         fish_add_path ~/.local/bin/hugo
 
         # amdgpu
-        fish_add_path /opt/rocm-6.3.4/bin
-        set HSA_OVERRIDE_GFX_VERSION 11.0.1
+        # fish_add_path /opt/rocm-6.3.4/bin
+        # set HSA_OVERRIDE_GFX_VERSION 11.0.1
 end
 
 # pyenv
